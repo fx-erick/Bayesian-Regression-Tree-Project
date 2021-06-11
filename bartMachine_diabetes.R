f00bc374c0ab99f27fc03df84e19ff639eb10cd2
Erick <erickfs@iis.fraunhofer.de>
2021-06-11 03:44:46 +0200

    Finished most of the BART results part of the report, updated the bartMachine script

--- bartMachine_diabetes.R
+++ bartMachine_diabetes.R
@@ -1,18 +1,60 @@
-options(java.parameters = "-Xmx2500m")
 library("bartMachine")
-set_bart_machine_num_cores(2)
+library("bart")
+
+options(java.parameters = "-Xmx2500m")
+set_bart_machine_num_cores(4)
 
 db = read.csv("http://www.rob-mcculloch.org/data/diabetes.csv")
+load("bart_machine.RData")
 
-X = data.frame(db[,2:11])
-y = db$y
-bart_machine <- bartMachine(X, y)
+#' 75% of the sample size
+smp_size <- floor(0.75 * nrow(db))
+
+#' set the seed to make your partition reproducible
+set.seed(123)
+
+train_ind <- sample(seq_len(nrow(db)), size = smp_size)
+
+train <- db[train_ind, ]
+test <- db[-train_ind, ]
+
+x_train <- train[,-c(1)]
+y_train <- train[,1]
+
+x_test <- test[,-c(1)]
+y_test <- test[,1]
+
+#train bart_machine from train datasets
+bart_machine <- bartMachine(x_train, y_train)
 bart_machine
+#check error asumptions and convergence diagnostics of the bart model
+check_bart_error_assumptions(bart_machine)
+plot_convergence_diagnostics(bart_machine)
+plot_y_vs_yhat(bart_machine, credible_intervals = TRUE)
+plot_y_vs_yhat(bart_machine, prediction_intervals = TRUE)
+#evaluate k_fold cross validation
+k_fold_cv(x_train, y_train, k_folds = 10)
+#plot rmse by number of trees
+rmse_by_num_trees(bart_machine, num_replicates = 10)
+#perform test set prediction of trained model
+oos_perf = bart_predict_for_test_data(bart_machine, x_test, y_test)
+print(oos_perf$rmse)
 
-k_fold_cv(X, y, k_folds = 10)
-rmse_by_num_trees(bart_machine, num_replicates = 20)
-
-bart_machine_cv <- bartMachineCV(X, y)
-k_fold_cv(X, y, k_folds = 10, k = 3, nu = 3, q = 0.99, num_trees = 200)
-yhat = predict(bart_machine_cv, X)
+#perform hyperparameter optimization to get the best
+#performing bart machine model according to k fold cv value
+bart_machine_cv <- bartMachineCV(x_train, y_train)
+bart_machine_cv
+#check error asumptions and convergence diagnostics of 
+#the bart model with optimized hyperparameter
 check_bart_error_assumptions(bart_machine_cv)
+plot_convergence_diagnostics(bart_machine_cv)
+#evaluate k_fold cross validation
+k_fold_cv(x_train, y_train, k_folds = 10, k = 3, nu = 3, q = 0.99, num_trees = 200)
+#perform test set prediction of trained model
+oos_perf = bart_predict_for_test_data(bart_machine_cv, x_test, y_test)
+print(oos_perf$rmse)
+plot_y_vs_yhat(bart_machine_cv, credible_intervals = TRUE)
+
+plot(oos_perf$y_hat,y_test)
+investigate_var_importance(bart_machine_cv, num_replicates_for_avg = 20)
+
